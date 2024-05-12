001b97f6d136b480cdfb3be9fd2822ce9dc6714c
Lumus <lumus.rb@gmail.com>
2024-05-12 17:40:31 +0500

    Создание activity c созданием заказа

--- app/src/main/java/com/example/homeworkers2/Services.java
+++ app/src/main/java/com/example/homeworkers2/Services.java
@@ -2,13 +2,17 @@ package com.example.homeworkers2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Services extends AppCompatActivity {
 
     private ImageButton stopButton;
+
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,20 @@ public class Services extends AppCompatActivity {
 
 
         stopButton = findViewById(R.id.stopButton);
+        nextButton = findViewById(R.id.service1);
 
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Services.this, CreateOrder.class);
+                startActivity(intent);
             }
         });
 
