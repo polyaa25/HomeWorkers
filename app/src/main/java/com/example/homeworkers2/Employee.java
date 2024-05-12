aef409404b3178d27687d3a087835b72363a9cf6
polyaa25 <krasnozhenovapolina@gmail.com>
2024-05-12 17:20:55 +0600

    начало создания активити рабочего

---
+++ app/src/main/java/com/example/homeworkers2/Employee.java
@@ -0,0 +1,14 @@
+package com.example.homeworkers2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee);
+    }
+}
