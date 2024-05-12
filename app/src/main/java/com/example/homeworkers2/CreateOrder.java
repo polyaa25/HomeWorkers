c551c348c37d74fd42ec12b6883c46a336f3cc8d
polyaa25 <krasnozhenovapolina@gmail.com>
2024-05-12 19:42:54 +0600

    создания вспывающего окна

--- app/src/main/java/com/example/homeworkers2/CreateOrder.java
+++ app/src/main/java/com/example/homeworkers2/CreateOrder.java
@@ -2,14 +2,19 @@ package com.example.homeworkers2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class CreateOrder extends AppCompatActivity {
 
 
     private ImageButton stopButton;
+    Animation fadeInAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,18 @@ public class CreateOrder extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        Button borderButton = findViewById(R.id.button_border);
+        borderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(fadeInAnimation); // Применение новой анимации при нажатии
+
+                // Переход к другой активности
+                Intent intent = new Intent(CreateOrder.this, Employee.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/homeworkers2/Employee.java
+++ app/src/main/java/com/example/homeworkers2/Employee.java
@@ -3,6 +3,13 @@ package com.example.homeworkers2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class Employee extends AppCompatActivity {
 
@@ -10,5 +17,43 @@ public class Employee extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee);
+
+        Button callButton = findViewById(R.id.employee_button);
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomSheetDialog();
+            }
+        });
+    }
+
+    public void showBottomSheetDialog() {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog);
+
+        Button callButton = bottomSheetDialog.findViewById(R.id.call_button);
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Button whatsappButton = bottomSheetDialog.findViewById(R.id.whatsapp_button);
+
+        callButton.setOnClickListener(v -> {
+            // Вызов телефона
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:+77057163397"));
+            startActivity(intent);
+            bottomSheetDialog.dismiss();
+        });
+
+        whatsappButton.setOnClickListener(v -> {
+            // Отправка сообщения в WhatsApp
+            String phoneNumber = "+77057163397";
+            String url = "https://api.whatsapp.com/send?phone=" + phoneNumber;
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+            bottomSheetDialog.dismiss();
+        });
+
+        bottomSheetDialog.show();
     }
 }
